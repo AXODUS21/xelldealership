da359303abe06ceed2eecff2b4f2c98a69028a26
Axellerosh <lubi.axellerosh@gmail.com>
2024-11-23 14:48:38 +0800

    fixing bootstrap

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,15 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ["bootstrap"], // Ensure bootstrap is included in the optimization process
+  resolve: {
+    alias: {
+      // Ensuring Bootstrap is correctly resolved in the build
+      bootstrap: "node_modules/bootstrap",
+    },
   },
   build: {
     rollupOptions: {
-      external: ["bootstrap"], // Mark 'bootstrap' as external to prevent bundling issues
+      external: ["bootstrap"], // If you want to externalize it
     },
   },
 });
