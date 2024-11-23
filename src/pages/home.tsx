81f1dc04d9532061825e9a56eac6938cf46866f6
Axellerosh <lubi.axellerosh@gmail.com>
2024-11-23 14:46:21 +0800

    fixing bootstrap

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,4 +1,5 @@
 import '../css/Home.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 import video from '../addons/carVid.mp4'
 
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  optimizeDeps: {
+    include: ["bootstrap"], // Ensure bootstrap is included in the optimization process
+  },
+  build: {
+    rollupOptions: {
+      external: ["bootstrap"], // Mark 'bootstrap' as external to prevent bundling issues
+    },
+  },
+});
